0ba947350acf45579859179d205c7c62381224ac
Leotrik <117585348+Leotrik@users.noreply.github.com>
2023-01-22 22:22:42 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,21 @@
+(function (){
+    const header = document.querySelector('.header');
+    window.onscroll = () => {
+        if (window.pageYOffset > 50) {
+            header.classList.add('header_active');
+        } else {
+            header.classList.remove('header_active');
+        }
+    };
+}());
+
+
+// Burger handler
+
+(function () {
+   const burgerItem = document.querySelector('.burger');
+   const menu = document.querySelector(',header-nav');
+   burgerItem.addEventListener('click', () => {
+    menu.classList.add('header-nav-active');
+   });
+}());
